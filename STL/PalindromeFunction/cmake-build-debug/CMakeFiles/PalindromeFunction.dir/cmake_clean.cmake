884033f99e36a2839cee30ed520aedcaf76ccc07
Sebastian <seberyincorporation@gmail.com>
2020-12-07 20:19:12 -0600

    Added: Palindrome Function

---
+++ STL/PalindromeFunction/cmake-build-debug/CMakeFiles/PalindromeFunction.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PalindromeFunction.dir/main.cpp.obj"
+  "PalindromeFunction.exe"
+  "PalindromeFunction.exe.manifest"
+  "PalindromeFunction.pdb"
+  "libPalindromeFunction.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PalindromeFunction.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
