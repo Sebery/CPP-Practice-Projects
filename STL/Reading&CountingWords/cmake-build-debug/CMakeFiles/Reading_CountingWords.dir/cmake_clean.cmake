ab8093a5d79d347587b9d346d18c4450df5ae909
Sebastian <seberyincorporation@gmail.com>
2020-12-08 19:23:18 -0600

    Added: Reading & Counting Words

---
+++ STL/Reading&CountingWords/cmake-build-debug/CMakeFiles/Reading_CountingWords.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Reading_CountingWords.dir/main.cpp.obj"
+  "Reading_CountingWords.exe"
+  "Reading_CountingWords.exe.manifest"
+  "Reading_CountingWords.pdb"
+  "libReading_CountingWords.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Reading_CountingWords.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
